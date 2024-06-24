efcab821bdb8a7b8961943b49cbbb7791a6e2dc2
apire <111300928+realapire@users.noreply.github.com>
2024-06-24 05:34:49 +0200

    Update content.js

--- content.js
+++ content.js
@@ -98,7 +98,7 @@ window.addEventListener('load', async function () {
 
         if (element) {
             const childElems = element.querySelectorAll('[class^="styles_control__"]');
-            if (childElems.length != 5) {
+            if (childElems.length != 5 && childElems.length != 2) {
                 const pinImg = chrome.runtime.getURL('assets/view.png');
                 const viewImg = chrome.runtime.getURL('assets/pin.png');
                 element.innerHTML += `<a href="#" class="styles_control__a" id="tellLocation" style="margin-bottom: 1rem; position: relative; touch-action: pan-x pan-y; background: rgba(0, 0, 0, .6);border: 0;border-bottom: .0625rem solid rgba(0, 0, 0, .4);cursor: pointer;height:40px;display: flex; align-items: center; justify-content: center;width: 40px;border-radius: 50%"><img alt='Return to start' loading='lazy' width='22' height='24' decoding='async' data-nimg='1' style="filter: invert(1); position: absolute;" class='styles_iconReturnToStart__PT25v' src='${viewImg}' style='color: transparent;'></button><div class='tooltip_tooltip__CHe2s tooltip_right__07M2V tooltip_roundnessXS__khTx4 tooltip_hideOnXs__hsJpx' style='top: 50%; transform: translateY(-50%) scale(0); opacity: 0; visibility: hidden;'>Return to start (R)<div class='tooltip_arrow__Rz_22'></div></div>`;
